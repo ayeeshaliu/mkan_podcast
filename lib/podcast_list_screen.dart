d737b85a5b4939b2cebe2325d2f417d89f0413f2
aishaaliu <aisha.aliu03@gmail.com>
2022-06-28 15:55:56 +0100

    changed size to 25

--- lib/podcast_list_screen.dart
+++ lib/podcast_list_screen.dart
@@ -35,7 +35,7 @@ class PodcastListScreen extends StatelessWidget {
                         icon: Icon(Icons.arrow_back),),
 
         IconButton(
-          iconSize: 30,
+          iconSize: 25,
             color: Colors.black,
             onPressed: (){},
             icon: Icon(Icons.all_inclusive),),
