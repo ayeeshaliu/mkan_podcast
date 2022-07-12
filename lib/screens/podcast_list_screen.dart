b7e979da35f142eb06013f8e78b7497fdf10d027
aishaaliu <aisha.aliu03@gmail.com>
2022-07-12 23:54:51 +0100

    initial commit

--- lib/screens/podcast_list_screen.dart
+++ lib/screens/podcast_list_screen.dart
@@ -19,51 +19,54 @@ class PodcastListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.45,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
 
-        Padding(padding: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
-              child: Row(
-                //crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    iconSize: 30,
-                    color: Colors.black,
-                      onPressed: () => Navigator.of(context).pop(),
-                      icon: Icon(Icons.arrow_back),),
+          Row(
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                iconSize: 30,
+                color: Colors.black,
+                  onPressed: () => Navigator.of(context).pop(),
+                  icon: Icon(Icons.arrow_back),),
 
       IconButton(
-        iconSize: 30,
-          color: Colors.black,
-          onPressed: (){},
-          icon: Icon(LineIcons.horizontalEllipsis),),
-                ],
-              ),
-            ),
-            //SizedBox(height: 30,),
-            Padding(padding: EdgeInsets.symmetric(vertical: 16, horizontal: 30),
-              child: Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(3, 180, 97, 1),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    height: 110,
-                    width: 110,
+          iconSize: 30,
+            color: Colors.black,
+            onPressed: (){},
+            icon: Icon(LineIcons.horizontalEllipsis),),
+            ],
+          ),
+              //SizedBox(height: 30,),
+              Padding(padding: EdgeInsets.symmetric(vertical: 16, horizontal: 30),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(3, 180, 97, 1),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        height: 110,
+                        width: 110,
 
-                  ),
-                  SizedBox(width: 32,),
-                  Container(
-                    child: Column(
+                      ),
+                    ),
+                    SizedBox(width: 32,),
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       //mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Container(
                           padding: EdgeInsets.only(bottom: 15),
-                          child: Text("The Joe\nExperience at...",
+                          child: Text("The Joe Experience at",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
 
                             style: TextStyle(
                               fontSize: 28,
@@ -71,77 +74,73 @@ class PodcastListScreen extends StatelessWidget {
                             ),
                           ),
                         ),
+                        Text(author,
+                        textAlign: TextAlign.start,
+                        ),
+
                         Container(
-                          padding: EdgeInsets.only(bottom: 5),
-                          child: Text(author,
-                          textAlign: TextAlign.start,
+                          padding: EdgeInsets.only(top: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                children: [
+                                  IconButton(onPressed: (){}, icon: Icon(LineIcons.soundcloud, size: 27,color: Color.fromRGBO(3, 180, 97, 1),),
+                                  ),
+                                  Text("10 Episodes"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  IconButton(onPressed: (){}, icon: Icon(Icons.share_outlined,size: 27, color: Color.fromRGBO(3, 180, 97, 1),),
+                                  ),
+                                  Text("Shares"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border_outlined, size:27,color: Color.fromRGBO(3, 180, 97, 1),),
+                                  ),
+                                  Text("Favourites"),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
 
                       ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    children: [
-                      IconButton(onPressed: (){}, icon: Icon(LineIcons.soundcloud, size: 27,color: Color.fromRGBO(3, 180, 97, 1),),
-                      ),
-                      Text("10 Episodes"),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      IconButton(onPressed: (){}, icon: Icon(Icons.share_outlined,size: 27, color: Color.fromRGBO(3, 180, 97, 1),),
-                      ),
-                      Text("Shares"),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border_outlined, size:27,color: Color.fromRGBO(3, 180, 97, 1),),
-                      ),
-                      Text("Favourites"),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 50),
-              child: Text("Episodes",
+
+              Text("Episodes",
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.w700,
                 ),
 
               ),
-            ),
 
-            Container(
-              height: 392,
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                children: RECOMMENDATION_LIST.map((epsd) => RecommendationItem(
-                    epsd.icon,
-                    epsd.duration,
-                    epsd.title,
-                    epsd.author,
-                    epsd.color,
-                    epsd.colors
-                )).toList(),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: RECOMMENDATION_LIST.map((epsd) => RecommendationItem(
+                      epsd.icon,
+                      epsd.duration,
+                      epsd.title,
+                      epsd.author,
+                      epsd.color,
+                      epsd.colors
+                  )).toList(),
 
+                ),
               ),
-            ),
 
 
-          ],
+            ],
+          ),
         ),
       ),
     );
